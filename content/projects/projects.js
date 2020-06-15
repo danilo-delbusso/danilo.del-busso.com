e7967fb772fe4d43833d1e80e2d228b8e17d04f1
dbus <dbus@h-q.cc>
2020-06-15 22:18:36 +0100

    Add personal projects

--- content/projects/projects.js
+++ content/projects/projects.js
@@ -1,30 +1,30 @@
 export default [
   {
-    title: "Alpava",
-    path: "https://github.com/adrianmarkperea/phrase-guesser-demo",
+    title: "Alpaca",
+    path: "https://github.com/danilo-delbusso/alpaca",
     description:
-      "A phrase guesser used to illustrate genetic algorithms. Written using React and Genie.js",
-    tags: ["JavaScript", "Genetic Algorithms"],
+      "The application merges the audio file in a video and a pdf to text, in order to produce a summary of the contents. It is meant to be used as a starting point for students who want to revise a particular lecture.",
+    tags: ["<a href='https://devpost.com/software/alpaca-074ags'>🏆 1st Place Royal Hackaway</a>","Java", "NLP"],
   },
   {
-    title: "Genie.js",
-    path: "https://github.com/adrianmarkperea/genie",
+    title: "easy nákup",
+    path: "https://github.com/lukakralj/easyNakup",
     description:
-      "A highly flexible, data-agnostic, and UI-independent Genetic Algorithm library",
-    tags: ["JavaScript", "Genetic Algorithms"],
+      "Dragonboard 410c based text recognisition app for elderly users in remote areas. Aimed at ease online shopping and enable local business to compete by bypassing online portal costs.",
+    tags: ["<a href='https://devpost.com/software/easy-nakup'>🏆 1st Place Hack Košice 2019</a>","Dragonboard 410c", "Node.js","MongooseDB", "socket.io"],
   },
   {
-    title: "GA Flappy Bird",
-    path: "https://github.com/adrianmarkperea/flappy-bird-genetic-algo-java",
+    title: "traffiK",
+    path: "https://github.com/danilo-delbusso/trafficK",
     description:
       "AI learns how to play flappy bird using neural networks and genetic algorithms",
-    tags: ["Java", "Processing", "Genetic Algorithms", "Neural Networks"],
+    tags: ["<a href='https://devpost.com/software/traffik'>🥉 3rd Place Hacktrain 5.0</a>","R", "Python", "JavaScript"],
   },
   {
-    title: "Headless Web Scraper",
-    path: "https://github.com/adrianmarkperea/betting-directory-scraper",
+    title: "Antichi Sapori Saperi Antichi",
+    path: "https://github.com/danilo-delbusso/AntichiSaporiSaperiAntichi",
     description:
-      "A headless web scraper for a betting directory. Uses Selenium and headless chromium.",
-    tags: ["Python", "Web Scraping"],
+      "The Scientific Lyceum 'A.Romita' of Campobasso presented at the MILAN EXPO 2015 this videogame in order to share the traditions of the region of Molise, Italy. The game narrates the past through a modern medium, with the goal of sharing the local culture with a videogame.",
+    tags: ["<a href='https://www.ansa.it/molise/notizie/2015/05/24/gioco-delloca-in-chiave-molisana_0bd04e70-8487-475a-a7ed-bc67d2b0e438.html'>🏆 Nationwide Winner</a>","Java"],
   },
 ]
