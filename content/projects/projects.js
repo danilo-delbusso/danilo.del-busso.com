ee9fb78ebae853ec8ef4108eace3968713da0cd9
dbus <dbus@h-q.cc>
2020-08-20 12:55:37 +0100

    Reorder projects

--- content/projects/projects.js
+++ content/projects/projects.js
@@ -4,41 +4,65 @@ export default [
     path: "https://github.com/danilo-delbusso/alpaca",
     description:
       "The application merges the audio file in a video and a pdf to text, in order to produce a summary of the contents. It is meant to be used as a starting point for students who want to revise a particular lecture.",
-    tags: ["<a href='https://devpost.com/software/alpaca-074ags'>🏆 1st Place Royal Hackaway</a>","Java&nbsp;<i class='fab fa-java'></i>", "NLP"],
-  },
-  {
-    title: "easy nákup",
-    path: "https://github.com/lukakralj/easyNakup",
-    description:
-      "Dragonboard 410c based text recognisition app for elderly users in remote areas. Aimed at ease online shopping and enable local business to compete by bypassing online portal costs.",
-    tags: ["<a href='https://devpost.com/software/easy-nakup'>🏆 1st Place Hack Košice 2019</a>","Dragonboard 410c", "Node.js&nbsp;<i class='fab fa-node-js'></i>","MongooseDB&nbsp;<i class='fas fa-database'></i>", "socket.io"],
+    tags: [
+      "<a href='https://devpost.com/software/alpaca-074ags'>🏆 1st Place Royal Hackaway</a>",
+      "Java&nbsp;<i class='fab fa-java'></i>",
+      "NLP",
+    ],
   },
   {
     title: "Blood Test Diary",
     path: "https://github.com/danilo-delbusso/bloodtest-diary",
     description:
       "A full stack SQL/Node.js/React diary system for blood tests and patient information.",
-    tags: ["MySQL&nbsp;<i class='fas fa-database'></i>", "React.js&nbsp;<i class='fab fa-react'></i>", "Node.js&nbsp;<i class='fab fa-node-js'></i>", "Electron"],
+    tags: [
+      "MySQL&nbsp;<i class='fas fa-database'></i>",
+      "React.js&nbsp;<i class='fab fa-react'></i>",
+      "Node.js&nbsp;<i class='fab fa-node-js'></i>",
+      "Electron",
+    ],
   },
+  {
+    title: "easy nákup",
+    path: "https://github.com/lukakralj/easyNakup",
+    description:
+      "Dragonboard 410c based text recognisition app for elderly users in remote areas. Aimed at ease online shopping and enable local business to compete by bypassing online portal costs.",
+    tags: [
+      "<a href='https://devpost.com/software/easy-nakup'>🏆 1st Place Hack Košice 2019</a>",
+      "Dragonboard 410c",
+      "Node.js&nbsp;<i class='fab fa-node-js'></i>",
+      "MongooseDB&nbsp;<i class='fas fa-database'></i>",
+      "socket.io",
+    ],
+  },
+
   {
     title: "traffiK",
     path: "https://github.com/danilo-delbusso/trafficK",
     description:
       "A train dwell time visual diagnostic tool using SWR dwelling time",
-    tags: ["<a href='https://devpost.com/software/traffik'>🥉 3rd Place Hacktrain 5.0</a>","R", "Python&nbsp;<i class='fab fa-python'></i>", "JavaScript&nbsp;<i class='fab fa-js-square'></i>"],
-  },
-  {
-    title: "Antichi Sapori Saperi Antichi",
-    path: "https://github.com/danilo-delbusso/AntichiSaporiSaperiAntichi",
-    description:
-      "The Scientific Lyceum 'A.Romita' of Campobasso presented at the MILAN EXPO 2015 this videogame in order to share the traditions of the region of Molise, Italy. The game narrates the past through a modern medium, with the goal of sharing the local culture with a videogame.",
-    tags: ["<a href='https://www.ansa.it/molise/notizie/2015/05/24/gioco-delloca-in-chiave-molisana_0bd04e70-8487-475a-a7ed-bc67d2b0e438.html'>🏆 Nationwide Winner</a>","Java <i class='fab fa-java'></i>"],
+    tags: [
+      "<a href='https://devpost.com/software/traffik'>🥉 3rd Place Hacktrain 5.0</a>",
+      "R",
+      "Python&nbsp;<i class='fab fa-python'></i>",
+      "JavaScript&nbsp;<i class='fab fa-js-square'></i>",
+    ],
   },
   {
     title: "Toguz Korgol",
     path: "https://github.com/danilo-delbusso/toguz-korgol-java",
     description:
       "A Java implementation of the <a href='https://en.wikipedia.org/wiki/Toguz_korgol'>Toguz Korgol</a> game with a simple AI opponent.",
-    tags: ["Game","Java <i class='fab fa-java'></i>","Gradle"],
+    tags: ["Game", "Java <i class='fab fa-java'></i>", "Gradle"],
+  },
+  {
+    title: "Antichi Sapori Saperi Antichi",
+    path: "https://github.com/danilo-delbusso/AntichiSaporiSaperiAntichi",
+    description:
+      "The Scientific Lyceum 'A.Romita' of Campobasso presented at the MILAN EXPO 2015 this videogame in order to share the traditions of the region of Molise, Italy. The game narrates the past through a modern medium, with the goal of sharing the local culture with a videogame.",
+    tags: [
+      "<a href='https://www.ansa.it/molise/notizie/2015/05/24/gioco-delloca-in-chiave-molisana_0bd04e70-8487-475a-a7ed-bc67d2b0e438.html'>🏆 Nationwide Winner</a>",
+      "Java <i class='fab fa-java'></i>",
+    ],
   },
 ]
