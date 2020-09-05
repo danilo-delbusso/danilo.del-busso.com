5608cb07fae5c93073c3cfea2c030e88fa38202b
Danilo Del Busso <danilo.delbusso1@gmail.com>
2020-09-05 18:16:01 +0100

    Add published articles section

---
+++ src/pages/articles.js
@@ -0,0 +1,46 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import projectsStyles from "../css/projects.module.css"
+import articles from "../../content/articles/articles"
+import Item from "../components/item"
+
+const Projects = () => {
+  return (
+    <Layout>
+      <SEO title="Published Articles" />
+      <section className="page-section">
+        <div className="container">
+          <h2 className="title">Published Articles</h2>
+          {articles.map(article => {
+            return (
+              <Item
+                key={article.title}
+                title={article.title}
+                path={article.path}
+                isInternal={false}
+                headerContent={
+                  <>
+                    {article.description}
+                    <br />
+                    <br />
+                    <div className={projectsStyles.tagContainer}>
+                      {article.tags.map(tag => (
+                        <small
+                          key={`${article.title}_${tag}`}
+                          dangerouslySetInnerHTML={{ __html: tag }}
+                        ></small>
+                      ))}
+                    </div>
+                  </>
+                }
+              />
+            )
+          })}
+        </div>
+      </section>
+    </Layout>
+  )
+}
+
+export default Projects
